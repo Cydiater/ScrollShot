4e9d97af090c0e615e6a01cfcf8404896edd1eb3
Cydiater <cydiater@gmail.com>
2021-09-17 21:55:34 +0800

    Initial Commit

---
+++ Shared/ScrollShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollShotApp.swift
+//  Shared
+//
+//  Created by Cydiater on 2021/9/17.
+//
+
+import SwiftUI
+
+@main
+struct ScrollShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
